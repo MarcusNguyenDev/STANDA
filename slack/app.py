7e265a7aeb3d5aa032c0f4358777a4948664db8b
ScriptSmith <ScriptSmith@users.noreply.github.com>
2023-05-13 11:20:07 +1000

    Simple langchain integration

--- slack/app.py
+++ slack/app.py
@@ -2,6 +2,30 @@ import os
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
+from langchain.chat_models import ChatOpenAI
+from langchain import PromptTemplate, LLMChain
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    AIMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage
+)
+
+chat = ChatOpenAI(temperature=0)
+CHAT_CONTEXT = [
+    SystemMessage(content="You are a helpful assistant named STANDA, that tells people what they were doing before stand-up meetings"),
+]
+
+def get_message_response(message_text):
+    response = chat(CHAT_CONTEXT + [HumanMessage(content=message_text)])
+    print(f"{response=}")
+    return response.content
+
 # Initializes your app with your bot token and socket mode handler
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
@@ -11,13 +35,15 @@ app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 @app.message()
 def message_hello(message, say):
     # say() sends a message to the channel where the event was triggered
-    print(message, say)
-    if "hello" in message['text'].lower():
-        say(f"Hello, <@{message['user']}> :wave:")
-    elif "bye" in message['text'].lower():
-        say(f"See ya later, <@{message['user']}> :wave:")
+    if "what was I doing" in message['text']:
+        say(get_message_response(message['text']))
+    elif "what am I doing" in message['text']:
+        say(get_message_response(message['text']))
+    elif "what are my blockers" in message['text']:
+        say(get_message_response(message['text']))
     else:
-        say(f"Sorry, <@{message['user']}> :wave:")
+        print("Did not match any of the conditions")
+        print(f"{message=} {say=}")
 
 # Start your app
 if __name__ == "__main__":
